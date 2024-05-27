c7047112595619a4f9515fc52ee91a482ce8504d
Oleg Kovalov <oleg@hey.com>
2024-05-27 14:47:36 +0200

    stricted verification

--- verify.go
+++ verify.go
@@ -13,7 +13,7 @@ const DefaultHeightThreshold uint64 = 80000 // ~ 14 days of 15 second headers
 // Verify verifies untrusted Header against trusted following general Header checks and
 // custom user-specific checks defined in Header.Verify.
 //
-// If trusted header is zero, no error is returned.
+// If trusted or untrstd header is zero, error is returned.
 // If heightThreshold is zero, uses DefaultHeightThreshold.
 // Always returns VerifyError.
 func Verify[H Header[H]](trstd, untrstd H, heightThreshold uint64) error {
@@ -51,6 +51,9 @@ func verify[H Header[H]](trstd, untrstd H, heightThreshold uint64) error {
 		heightThreshold = DefaultHeightThreshold
 	}
 
+	if trstd.IsZero() {
+		return ErrZeroHeader
+	}
 	if untrstd.IsZero() {
 		return ErrZeroHeader
 	}
