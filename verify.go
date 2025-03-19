645ecabcd17e5b170ed85c75f536702feebb5fc5
rene <41963722+renaynay@users.noreply.github.com>
2025-03-19 14:58:41 +0100

    fix(verify.go): Re-order the checks in order of sanity-check

--- verify.go
+++ verify.go
@@ -53,6 +53,11 @@ func verify[H Header[H]](trstd, untrstd H) error {
 		return fmt.Errorf("%w: '%s' != '%s'", ErrWrongChainID, untrstd.ChainID(), trstd.ChainID())
 	}
 
+	known := untrstd.Height() <= trstd.Height()
+	if known {
+		return fmt.Errorf("%w: '%d' <= current '%d'", ErrKnownHeader, untrstd.Height(), trstd.Height())
+	}
+
 	if untrstd.Time().Before(trstd.Time()) {
 		return fmt.Errorf("%w: timestamp '%s' < current '%s'", ErrUnorderedTime, formatTime(untrstd.Time()), formatTime(trstd.Time()))
 	}
@@ -60,11 +65,6 @@ func verify[H Header[H]](trstd, untrstd H) error {
 	now := time.Now()
 	if untrstd.Time().After(now.Add(clockDrift)) {
 		return fmt.Errorf("%w: timestamp '%s' > now '%s', clock_drift '%v'", ErrFromFuture, formatTime(untrstd.Time()), formatTime(now), clockDrift)
-	}
-
-	known := untrstd.Height() <= trstd.Height()
-	if known {
-		return fmt.Errorf("%w: '%d' <= current '%d'", ErrKnownHeader, untrstd.Height(), trstd.Height())
 	}
 
 	return nil
