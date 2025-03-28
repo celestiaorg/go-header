77e7ea532d8a7fb1569768ab1021cde59020ffe3
Wondertan <hlibwondertan@gmail.com>
2025-03-28 19:12:57 +0100

    fix test

--- p2p/subscription_test.go
+++ p2p/subscription_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/celestiaorg/go-header/headertest"
 )
 
-// TestSubscriber tests the header Module's implementation of Subscriber.
+// TestSubscriber a simple test to check if the subscriber can receive headers from the network.
 func TestSubscriber(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
 	defer cancel()
@@ -73,23 +73,24 @@ func TestSubscriber(t *testing.T) {
 	}
 
 	// subscribe
-	_, err = p2pSub2.Subscribe()
+	senderSubscription, err := p2pSub2.Subscribe()
 	require.NoError(t, err)
 
 	subscription, err := p2pSub1.Subscribe()
 	require.NoError(t, err)
 
 	expectedHeader := suite.GenDummyHeaders(1)[0]
-	bin, err := expectedHeader.MarshalBinary()
-	require.NoError(t, err)
-
-	err = p2pSub2.topic.Publish(ctx, bin, pubsub.WithReadiness(pubsub.MinTopicSize(1)))
+	err = p2pSub2.Broadcast(ctx, expectedHeader, pubsub.WithReadiness(pubsub.MinTopicSize(1)))
 	require.NoError(t, err)
 
 	// get next Header from network
 	header, err := subscription.NextHeader(ctx)
 	require.NoError(t, err)
+	assert.Equal(t, expectedHeader.Height(), header.Height())
+	assert.Equal(t, expectedHeader.Hash(), header.Hash())
 
+	header, err = senderSubscription.NextHeader(ctx)
+	require.NoError(t, err)
 	assert.Equal(t, expectedHeader.Height(), header.Height())
 	assert.Equal(t, expectedHeader.Hash(), header.Hash())
 }
