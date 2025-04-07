510b17b1d97be3274639e64808516b634cc24b4f
IliaBulavintsev <twintipper@mail.ru>
2025-04-07 20:27:48 +0800

    feat(sync): non-recursive singleflight protection for Head (#229)
    Closes: https://github.com/celestiaorg/go-header/issues/154
    
    This PR adds singleflight protection to prevent duplicate header
    requests in the Exchange component. The focus is on minimizing redundant
    requests, especially around the Head method, which is most likely to
    experience simultaneous calls.
    
    I moved out single-flight protection out of exchange to be in sync.Syncer. This way the single-flight protection works for every Head implementation: p2p, core. Besides, I avoid using the x/sync/singleflight as it does not handle context cancellation.
    
    This also fixes an extremely rare but possible issue with subjective initialization similar to #247 (cc @walldiss). This issue is caused by to be removed syncGetter when used together with recursion. The fix is to move singleflight handling out of sync.Syncer, it should not be responsible for this. This way we fix the issue, without the need for read/write refactoring in sync_head.go we discussed.
    
    ---------
    
    Co-authored-by: Wondertan <hlibwondertan@gmail.com>
    Co-authored-by: IliaBulavintsev <ibulavincev@finch-melrose.com>

---
+++ sync/head_sync_test.go
@@ -0,0 +1,48 @@
+package sync
+
+import (
+	"context"
+	"math/rand/v2"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	libhead "github.com/celestiaorg/go-header"
+	"github.com/celestiaorg/go-header/headertest"
+)
+
+func Test_syncHead(t *testing.T) {
+	counter := &headCounter{}
+	sh := &syncHead[*headertest.DummyHeader]{head: counter}
+
+	callsN := 1000
+	wg := sync.WaitGroup{}
+	wg.Add(callsN)
+	for i := 0; i < callsN; i++ {
+		go func() {
+			defer wg.Done()
+			time.Sleep(time.Duration(rand.IntN(1000)) * time.Microsecond)
+			_, _ = sh.Head(context.Background())
+		}()
+	}
+
+	wg.Wait()
+
+	assert.Less(t, int(counter.cntr.Load()), callsN/100) // <1% of calls should go through
+}
+
+type headCounter struct {
+	cntr atomic.Int64
+}
+
+func (h *headCounter) Head(
+	ctx context.Context,
+	h2 ...libhead.HeadOption[*headertest.DummyHeader],
+) (*headertest.DummyHeader, error) {
+	time.Sleep(time.Millisecond * 1)
+	h.cntr.Add(1)
+	return &headertest.DummyHeader{}, nil
+}
