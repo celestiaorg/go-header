d37c42be43f7523b2d16c15f64cadcc3aa9e72ae
Wondertan <hlibwondertan@gmail.com>
2025-05-08 13:47:59 +0200

    tests for reinit

--- sync/sync_head_test.go
+++ sync/sync_head_test.go
@@ -29,13 +29,19 @@ func TestSyncer_TailInit(t *testing.T) {
 	require.NoError(t, err)
 
 	tests := []struct {
-		name     string
-		option   Option
-		expected func() *headertest.DummyHeader
+		name                 string
+		option               Option
+		expected             func() *headertest.DummyHeader
+		expectedAfterRestart func() *headertest.DummyHeader
 	}{
 		{
 			"Estimate",
 			func(p *Parameters) {}, // noop to trigger estimation,
+			func() *headertest.DummyHeader {
+				remoteTail, err := remoteStore.Tail(ctx)
+				require.NoError(t, err)
+				return remoteTail
+			},
 			func() *headertest.DummyHeader {
 				remoteTail, err := remoteStore.Tail(ctx)
 				require.NoError(t, err)
@@ -48,11 +54,21 @@ func TestSyncer_TailInit(t *testing.T) {
 			func() *headertest.DummyHeader {
 				return expectedTail
 			},
+			func() *headertest.DummyHeader {
+				expectedTail, err := remoteStore.GetByHeight(ctx, expectedTail.Height()+10)
+				require.NoError(t, err)
+				return expectedTail
+			},
 		},
 		{
 			"SyncFromHeight",
 			WithSyncFromHeight(expectedTail.Height()),
 			func() *headertest.DummyHeader {
+				return expectedTail
+			},
+			func() *headertest.DummyHeader {
+				expectedTail, err := remoteStore.GetByHeight(ctx, expectedTail.Height()-10)
+				require.NoError(t, err)
 				return expectedTail
 			},
 		},
@@ -61,7 +77,7 @@ func TestSyncer_TailInit(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			ds := dssync.MutexWrap(datastore.NewMapDatastore())
-			localStore, err := store.NewStore[*headertest.DummyHeader](ds)
+			localStore, err := store.NewStore[*headertest.DummyHeader](ds, store.WithWriteBatchSize(1))
 			require.NoError(t, err)
 			err = localStore.Start(ctx)
 			require.NoError(t, err)
@@ -77,18 +93,30 @@ func TestSyncer_TailInit(t *testing.T) {
 
 			err = syncer.Start(ctx)
 			require.NoError(t, err)
-
 			time.Sleep(time.Millisecond * 100)
 
+			// check that the syncer has the expected tail and head
 			expectedTail := test.expected()
-
 			storeTail, err := localStore.Tail(ctx)
 			require.NoError(t, err)
 			assert.EqualValues(t, expectedTail.Height(), storeTail.Height())
-
 			storeHead, err := localStore.Head(ctx)
 			require.NoError(t, err)
 			assert.EqualValues(t, remoteStore.Height(), storeHead.Height())
+
+			// restart the Syncer and set a new tail
+			err = syncer.Stop(ctx)
+			require.NoError(t, err)
+			expectedTail = test.expectedAfterRestart()
+			syncer.Params.SyncFromHeight = expectedTail.Height()
+			syncer.Params.SyncFromHash = expectedTail.Hash()
+			err = syncer.Start(ctx)
+			require.NoError(t, err)
+
+			// ensure that the Syncer moved to the new tail after restart
+			storeTail, err = localStore.Tail(ctx)
+			require.NoError(t, err)
+			assert.EqualValues(t, expectedTail.Height(), storeTail.Height())
 		})
 	}
 }
