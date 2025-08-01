9b03087e89dec2e73ca203c7a38b32028e9d485f
Hlib Kanunnikov <hlibwondertan@gmail.com>
2025-08-01 22:10:43 +0200

    feat(store): store recovery tools (#325)
    Unsafe tools to manipulate the store's state

---
+++ store/store_recover.go
@@ -0,0 +1,77 @@
+package store
+
+import (
+	"context"
+	"errors"
+
+	"github.com/ipfs/go-datastore"
+
+	"github.com/celestiaorg/go-header"
+)
+
+// ResetTail resets the tail of the store to be at the given height.
+// The new tail must be present in the store.
+// WARNING: Only use this function if you know what you are doing.
+func UnsafeResetTail[H header.Header[H]](
+	ctx context.Context,
+	store *Store[H],
+	height uint64,
+) error {
+	if err := store.setTail(ctx, store.ds, height); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ResetHead resets the head of the store to be at the given height.
+// The new head must be present in the store.
+// WARNING: Only use this function if you know what you are doing.
+func UnsafeResetHead[H header.Header[H]](
+	ctx context.Context,
+	store *Store[H],
+	height uint64,
+) error {
+	newHead, err := store.getByHeight(ctx, height)
+	if err != nil {
+		return err
+	}
+
+	if err := writeHeaderHashTo(ctx, store.ds, newHead, headKey); err != nil {
+		return err
+	}
+
+	store.contiguousHead.Store(&newHead)
+	return nil
+}
+
+// FindHeader forward iterates over the store starting from the given height until it finds any stored header
+// or the context is canceled.
+func FindHeader[H header.Header[H]](
+	ctx context.Context,
+	store *Store[H],
+	startFrom uint64,
+) (hdr H, err error) {
+	ctx, done := store.withReadTransaction(ctx)
+	defer done()
+
+	for height := startFrom; ctx.Err() == nil; height++ {
+		hash, err := store.heightIndex.HashByHeight(ctx, height, false)
+		if errors.Is(err, datastore.ErrNotFound) {
+			continue
+		}
+		if err != nil {
+			return hdr, err
+		}
+
+		ok, err := store.Has(ctx, hash)
+		if err != nil {
+			return hdr, err
+		}
+		if ok {
+			return store.Get(ctx, hash)
+		}
+	}
+
+	return hdr, ctx.Err()
+}
